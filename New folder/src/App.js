9db5d7dad658b8d361301478d0a8c778b7d4e2c8
boomi1709 <boomibalan7766@gmail.com>
2022-07-27 17:01:51 +0530

    update

---
+++ New folder/src/App.js
@@ -0,0 +1,22 @@
+import Task2 from "./Boomi/Task2";
+
+import Task3 from "./Boomi/Task3";
+
+// import Task1 from "./Boomi/Task1";
+
+
+function App() {
+  return (
+    <div>
+      <header>
+            {/* <Task1/> */}
+                
+           <Task2/>
+
+            {/* <Task3/> */}
+          </header>
+    </div>
+  );
+}
+
+export default App;
